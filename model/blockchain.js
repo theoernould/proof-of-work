3bf865b9812fddb23f6aa873a7a018ccae23892c
Théo ERNOULD <theo.ernould@outlook.fr>
2025-02-09 17:51:38 +0100

    feat: improve blockchain validation and formatting

--- model/blockchain.js
+++ model/blockchain.js
@@ -1,9 +1,10 @@
+// Import required modules
 var addressUtilities = require('../utils/address');
 var arrayUtilities = require('../utils/array');
 var validator = require('../utils/validator');
+var chainUtilities = require('../utils/chain');
 
-var blockchain = function blockchain(){
-
+var blockchain = function blockchain() {
   var self = this;
 
   this.init = init;
@@ -16,57 +17,38 @@ var blockchain = function blockchain(){
   this.chain;
   this.currentTransactions;
 
-  function init(){
-    /*
-    *  initialize the blockchain, creating a new empty chain,
-    *  an empty transactions list and creating the first block
-    */
+  function init() {
     self.chain = [];
     self.currentTransactions = [];
-    self.newBlock(100, 1);
+    self.newBlock(100, "1");
   }
 
-  function getChain(){
-    /*
-    *  returns the chain
-    */
+  function getChain() {
     return self.chain;
   }
 
-  function mine(miner){
-    /*
-    *  implements the mining function. simple as is, it just
-    *  creates a new transaction with "sender" 0 to show that
-    *  this is a mined block.
-    */
-
-    var lastBlock = self.chain[self.chain.length-1];
-    var transaction = newTransaction(0,miner,1);
+  function mine(miner) {
+    var lastBlock = self.chain[self.chain.length - 1];
+    var transaction = newTransaction(0, miner, 1);
     var proof = validator.generateProof(transaction);
     var previousHash = validator.calculateHash(lastBlock.transaction[0]);
     return newBlock(proof, previousHash);
   }
 
-  function newBlock(proof, previousHash){
-    /*
-    *  Generate a new blocks and adds it to the chain
-    */
+  function newBlock(proof, previousHash) {
     var block = {
-      "index": self.chain.length+1,
+      "index": self.chain.length + 1,
       "timestamp": new Date().getTime(),
       "transaction": self.currentTransactions,
       "proof": proof,
       "previousHash": previousHash
-    }
+    };
     self.currentTransactions = [];
     self.chain.push(block);
     return block;
   }
 
-  function newTransaction(sender, receiver, amount){
-    /*
-    *  Generate a new transaction
-    */
+  function newTransaction(sender, receiver, amount) {
     var transaction = {
       sender: sender,
       receiver: receiver,
@@ -76,24 +58,44 @@ var blockchain = function blockchain(){
     return transaction;
   }
 
-  function checkChain(){
-    
+  function checkChain() {
+    if (self.chain.length === 0) return [];
+
+    for (var i = 1; i < self.chain.length; i++) {
+      var currentBlock = self.chain[i];
+      var previousBlock = self.chain[i - 1];
+
+      if (currentBlock.previousHash !== validator.calculateHash(previousBlock.transaction[0])) {
+        return [];
+      }
+
+      if (validator.generateProof(currentBlock.transaction[0]) !== currentBlock.proof) {
+        return [];
+      }
+
+      if (!currentBlock.index || !currentBlock.timestamp || !currentBlock.transaction ||
+          !currentBlock.proof || !currentBlock.previousHash) {
+        return [];
+      }
+
+      if (currentBlock.index !== previousBlock.index + 1) {
+        return [];
+      }
+    }
+    return self.chain;
   }
 
-
-  if(blockchain.caller != blockchain.getInstance){
-    throw new Error("This object cannot be instanciated");
+  if (blockchain.caller !== blockchain.getInstance) {
+    throw new Error("This object cannot be instantiated");
   }
-
 };
 
-
 blockchain.instance = null;
-blockchain.getInstance = function(){
-	if(this.instance === null){
-		this.instance = new blockchain();
-	}
-	return this.instance;
+blockchain.getInstance = function () {
+  if (this.instance === null) {
+    this.instance = new blockchain();
+  }
+  return this.instance;
 };
 
 module.exports = blockchain.getInstance();
